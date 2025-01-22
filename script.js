df626d15b8b5079518a586ae0d9b7c7c3713c5f6
Swarnava Bose <iamacoder9900@gmail.com>
2025-01-22 21:12:38 +0530

    1.09

--- script.js
+++ script.js
@@ -30,7 +30,6 @@ async function handleFormSubmit(event)
     console.log('Message:', message);
 
     const scriptURL = "APPS_SCRIPT_KEY";
-    console.log("scripturl : ",scriptURL);
     try {
         await fetch(scriptURL, {
             method: "POST",
